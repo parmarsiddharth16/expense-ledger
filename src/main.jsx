cbfb4b3ce99326ec4b26a7e97da231b776c3c791
parmarsiddharth16 <parmarsiddharth@gmail.com>
2026-06-25 17:49:43 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ExpenseLedger from "../ExpenseLedger.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ExpenseLedger />
+  </React.StrictMode>
+);
