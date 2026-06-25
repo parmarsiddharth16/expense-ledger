8cc3ecb3858c266e1c203eba1b02a4acf3d4e7b0
parmarsiddharth16 <parmarsiddharth@gmail.com>
2026-06-25 18:14:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ExpenseLedger from "../ExpenseLedger.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ExpenseLedger />
-  </React.StrictMode>
-);
+async function init() {
+  const { default: ExpenseLedger } = await import("../ExpenseLedger.jsx");
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <ExpenseLedger />
+    </React.StrictMode>
+  );
+}
+
+init();
